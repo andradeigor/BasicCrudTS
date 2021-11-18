ea3312f4e8bc202f74cc07ec67e2486758956c55
Igor <igor@pop-os.localdomain>
2021-11-18 16:49:58 -0300

    fixed the tsconfig and ts-node

--- build/src/index.js
+++ build/src/index.js
@@ -1,5 +1,10 @@
-import express from "express";
-const app = express();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const PORT = 8000;
 app.get("/", (req, res) => {
     res.json({ message: "hellor word" });
