719e9fe642d6e9cd3fdfdbbb3720dc6c64c3e9db
Igor <ig.vp@hotmail.com>
2021-11-30 22:47:06 -0300

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import UserRouter from "./router/user";
 import db from "./database/index";
+import cors from "cors";
 const app = express();
 const PORT: number = 8000;
 db();
+app.use(cors());
 app.use(express.json());
 app.use("/user", UserRouter);
 app.listen(PORT, (): void => {
